10b019429e783187c6c0fcd6a37f44dde2ec5c3e
Anushri <sastikaranushri@gmail.com>
2026-02-22 13:09:30 -0500

    changes in return statement

--- delete_duplicate_emails.py
+++ delete_duplicate_emails.py
@@ -9,7 +9,7 @@ person = pd.DataFrame({
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values(by='id', inplace=True)
     person.drop_duplicates(subset=['email'],inplace= True)
-    return person
+    return
 
 # using group by
 # def delete_duplicate_emails(person: pd.DataFrame) -> None:
